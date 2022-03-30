d15d6bb9045ac57a265b7f907dd13d28d161c7af
florent-le-maire <poursteam347@gmail.com>
2022-03-30 23:17:23 +0200

    develop2

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <FormConnect></FormConnect>
+</template>
+
+<script>
+import FormConnect from './components/FormConnect.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormConnect
+  }
+}
+</script>
+
+<style>
+html,
+body,
+#app{
+  height: 100%;
+}
+body {
+  background: -webkit-linear-gradient(aquamarine, blue);
+  background: -o-linear-gradient(aquamarine, blue);
+  background: -moz-linear-gradient(aquamarine, blue);
+  background: linear-gradient(aquamarine, blue);
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
